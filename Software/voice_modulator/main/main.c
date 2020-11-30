c8945074bc40dbe8b47878338a562b73be5ba194
Joey Strandnes <joey.strandnes@hotmail.com>
2020-11-30 15:00:08 +0100

    Test code

--- Software/voice_modulator/main/main.c
+++ Software/voice_modulator/main/main.c
@@ -12,7 +12,7 @@
 
 
 
-#define ADC_PIN ADC1_CHANNEL_0
+#define ADC_PIN ADC1_CHANNEL_6
 
 void ADC_SETUP(){
 
@@ -27,27 +27,36 @@ void app_main(void){
     ADC_SETUP();
 
     dac_output_enable(DAC_CHANNEL_1);
-    
-    uint16_t ADC_BUFFER[100];
+
+    //uint16_t ADC_BUFFER[100];
+    uint8_t DAC_BUFFER[10000];
     //uint16_t ADC_Read;
 
+    //uint16_t TEST16 = 0b0001101101110011;
+    //uint16_t TEST16 = 0b0000000011111111;
+    /*
+    uint8_t  TEST8 = 0;
+    TEST8 = (TEST16 >> 5);
+
+    printf("Hello world!\n");
+    printf("%d\n", TEST16);
+    printf("%d\n", TEST8);
+*/
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    for (uint8_t i = 0; i < 100; i++){
+      DAC_BUFFER[i] = (adc1_get_raw(ADC_PIN) >> 5);
+
+    }
 
     while(1) {
 
-      for (uint8_t i = 0; i < 100; i++){
-        ADC_BUFFER[i] = adc1_get_raw(ADC_PIN);
-      }
-
       for(uint8_t i = 0; i < 100; i++){
-        printf("%d", ADC_BUFFER[i]);
-
+        dac_output_voltage(DAC_CHANNEL_1, DAC_BUFFER[i]);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+        //printf("%d", DAC_BUFFER[i]);
       }
 
-      /*
-      ADC_Read = adc1_get_raw(ADC_PIN);
-      printf("ADC Value:");
-      printf("%d\n", ADC_Read);
-      */
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
